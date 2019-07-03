91dfbb1b05359e688d960a90c7893c734c0eea4b
Ammar Al-Helali <ammar0ah@gmail.com>
2019-07-04 01:01:23 +0300

    reports

---
+++ iOS/SurveyMaster/Controller/Responses and Reports/ReportsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ReportsViewController.swift
+//  SurveyMaster
+//
+//  Created by Ammar Al-Helali on 7/3/19.
+//  Copyright © 2019 Ammar Al-Helali. All rights reserved.
+//
+
+import UIKit
+
+class ReportsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
