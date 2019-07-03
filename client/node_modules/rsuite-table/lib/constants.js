5aeaca9b3470801c660a778c1eebab8948c735fa
Ammar Al-Helali <ammar0ah@gmail.com>
2019-07-03 09:55:50 +0300

    fixed a bug

--- client/node_modules/rsuite-table/lib/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var LAYER_WIDTH = exports.LAYER_WIDTH = 30;
-var SCROLLBAR_MIN_WIDTH = exports.SCROLLBAR_MIN_WIDTH = 20;
